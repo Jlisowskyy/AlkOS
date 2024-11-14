5df565195c7130f6e96dccf20f02e41b3a190fe0
Jakub Lisowski <Jlisowskyy@gmail.com>
2024-11-14 13:32:05 +0100

    Refacoted spacing in build cross compile

--- env/build_cross_compile.bash
+++ env/build_cross_compile.bash
@@ -18,62 +18,62 @@ source "${CROSS_COMPILE_BUILD_SCRIPT_DIR}/pretty_print.bash"
 source "${CROSS_COMPILE_BUILD_SCRIPT_DIR}/helpers.bash"
 
 help() {
-  echo "${CROSS_COMPILE_BUILD_SCRIPT_PATH} --install [--build_dir | -b <dir>] [--tool_dir | -t <dir>] [--verbose | -v]"
-  echo "Where:"
-  echo "--install         | -i - required flag to start installation"
-  echo "--build_dir <dir> | -b <dir> - provides directory <dir> to save all build files"
-  echo "--tool_dir  <dir> | -t <dir> - directory where tooling should be saved"
-  echo "--verbose         | -v - flag to enable verbose output"
+    echo "${CROSS_COMPILE_BUILD_SCRIPT_PATH} --install [--build_dir | -b <dir>] [--tool_dir | -t <dir>] [--verbose | -v]"
+    echo "Where:"
+    echo "--install         | -i - required flag to start installation"
+    echo "--build_dir <dir> | -b <dir> - provides directory <dir> to save all build files"
+    echo "--tool_dir  <dir> | -t <dir> - directory where tooling should be saved"
+    echo "--verbose         | -v - flag to enable verbose output"
 }
 
 runner() {
-  assert_argument_provided "$1"
+    assert_argument_provided "$1"
 
-  local dump_info="$1"
-  shift
+    local dump_info="$1"
+    shift
 
-  base_runner "${dump_info}" "${CROSS_COMPILE_BUILD_VERBOSE}" "$@"
+    base_runner "${dump_info}" "${CROSS_COMPILE_BUILD_VERBOSE}" "$@"
 }
 
 prepare_directory() {
-  assert_argument_provided "$1"
-  local dir="$1"
+    assert_argument_provided "$1"
+    local dir="$1"
 
-  if ! [ -d "${dir}" ]; then
-    pretty_info "Creating directory ${dir}"
-    mkdir -p "${dir}" || dump_error "Failed to create directory ${dir}"
-  else
-    pretty_info "Directory ${dir} already exists"
-  fi
+    if ! [ -d "${dir}" ]; then
+        pretty_info "Creating directory ${dir}"
+        mkdir -p "${dir}" || dump_error "Failed to create directory ${dir}"
+    else
+        pretty_info "Directory ${dir} already exists"
+    fi
 }
 
 download_source() {
-  assert_argument_provided "$1"
-  assert_argument_provided "$2"
+    assert_argument_provided "$1"
+    assert_argument_provided "$2"
 
-  local name="$1"
-  local link="$2"
+    local name="$1"
+    local link="$2"
 
-  if ! [ -f "${name}" ]; then
-    pretty_info "Downloading ${name}"
-    runner "Failed to download ${name}" wget "${link}"
-    pretty_success "${name} downloaded correctly"
-  else
-    pretty_info "Skipping... ${name} already downloaded"
-  fi
+    if ! [ -f "${name}" ]; then
+        pretty_info "Downloading ${name}"
+        runner "Failed to download ${name}" wget "${link}"
+        pretty_success "${name} downloaded correctly"
+    else
+        pretty_info "Skipping... ${name} already downloaded"
+    fi
 }
 
 download_extract_gnu_source() {
-  assert_argument_provided "$1"
-  assert_argument_provided "$2"
-  local name="$1"
-  local link="$2"
+    assert_argument_provided "$1"
+    assert_argument_provided "$2"
+    local name="$1"
+    local link="$2"
 
-  download_source "${name}" "${link}/${name}"
+    download_source "${name}" "${link}/${name}"
 
-  pretty_info "Extracting ${name}"
-  runner "Failed to extract ${name}" tar -xf "${name}"
-  pretty_success "${name} extracted correctly"
+    pretty_info "Extracting ${name}"
+    runner "Failed to extract ${name}" tar -xf "${name}"
+    pretty_success "${name} extracted correctly"
 }
 
 build_binutils() {
@@ -137,105 +137,105 @@ build_gcc() {
 }
 
 build_gdb() {
-  local gdb_dir="${CROSS_COMPILE_BUILD_BUILD_DIR}/build_gdb"
-  local gdb_name="gdb-${CROSS_COMPILE_BUILD_GDB_VER}"
+    local gdb_dir="${CROSS_COMPILE_BUILD_BUILD_DIR}/build_gdb"
+    local gdb_name="gdb-${CROSS_COMPILE_BUILD_GDB_VER}"
 
-  pretty_info "Building GDB"
+    pretty_info "Building GDB"
 
-  prepare_directory "${gdb_dir}"
-  cd "${gdb_dir}"
+    prepare_directory "${gdb_dir}"
+    cd "${gdb_dir}"
 
-  download_extract_gnu_source "${gdb_name}.tar.gz" "https://ftp.gnu.org/gnu/gdb/"
+    download_extract_gnu_source "${gdb_name}.tar.gz" "https://ftp.gnu.org/gnu/gdb/"
 
-  pretty_info "Configuring GDB"
-  runner "Failed to configure GDB" ${gdb_name}/configure --target=${CROSS_COMPILE_BUILD_TARGET} --prefix="${CROSS_COMPILE_BUILD_TOOL_DIR}" --disable-werror
-  pretty_success "GDB configured correctly"
+    pretty_info "Configuring GDB"
+    runner "Failed to configure GDB" ${gdb_name}/configure --target=${CROSS_COMPILE_BUILD_TARGET} --prefix="${CROSS_COMPILE_BUILD_TOOL_DIR}" --disable-werror
+    pretty_success "GDB configured correctly"
 
-  pretty_info "Building GDB with ${PROC_COUNT} threads"
-  runner "Failed to build GDB" make -j "${PROC_COUNT}" all-gdb
-  pretty_success "GDB built correctly"
+    pretty_info "Building GDB with ${PROC_COUNT} threads"
+    runner "Failed to build GDB" make -j "${PROC_COUNT}" all-gdb
+    pretty_success "GDB built correctly"
 
-  pretty_info "Installing GDB"
-  runner "Failed to install GDB" make install-gdb
-  pretty_success "GDB installed correctly"
+    pretty_info "Installing GDB"
+    runner "Failed to install GDB" make install-gdb
+    pretty_success "GDB installed correctly"
 
-  pretty_success "GDB build completed"
+    pretty_success "GDB build completed"
 }
 
 run_build() {
-  pretty_info "Starting GCC cross-compiler build with build directory: ${CROSS_COMPILE_BUILD_BUILD_DIR} and target directory: ${CROSS_COMPILE_BUILD_TOOL_DIR}"
+    pretty_info "Starting GCC cross-compiler build with build directory: ${CROSS_COMPILE_BUILD_BUILD_DIR} and target directory: ${CROSS_COMPILE_BUILD_TOOL_DIR}"
 
-  check_is_in_env_path "${CROSS_COMPILE_BUILD_TOOL_DIR}/bin"
-  local is_in_path=$?
+    check_is_in_env_path "${CROSS_COMPILE_BUILD_TOOL_DIR}/bin"
+    local is_in_path=$?
 
-  export PATH="${CROSS_COMPILE_BUILD_TOOL_DIR}/bin:${PATH}"
-  export PREFIX="${CROSS_COMPILE_BUILD_TOOL_DIR}"
+    export PATH="${CROSS_COMPILE_BUILD_TOOL_DIR}/bin:${PATH}"
+    export PREFIX="${CROSS_COMPILE_BUILD_TOOL_DIR}"
 
-  build_binutils
-  build_gdb
-  build_gcc
+    build_binutils
+    build_gdb
+    build_gcc
 
-  add_to_user_env_path "${CROSS_COMPILE_BUILD_TOOL_DIR}/bin" "${is_in_path}"
+    add_to_user_env_path "${CROSS_COMPILE_BUILD_TOOL_DIR}/bin" "${is_in_path}"
 
-  pretty_success "Build: ${CROSS_COMPILE_BUILD_SCRIPT_PATH} completed successfully"
+    pretty_success "Build: ${CROSS_COMPILE_BUILD_SCRIPT_PATH} completed successfully"
 }
 
 parse_args() {
-  while [[ $# -gt 0 ]]; do
-    case $1 in
-      -t|--tool_dir)
-        CROSS_COMPILE_BUILD_TOOL_DIR="$2"
-        shift
-        shift
-        ;;
-      -b|--build_dir)
-        CROSS_COMPILE_BUILD_BUILD_DIR="$2"
-        shift
-        shift
-        ;;
-      -h|--help)
-        help
-        exit 0
-        ;;
-      -i|--install)
-        CROSS_COMPILE_BUILD_INSTALL_FOUND=true
-        shift
-        ;;
-      -v|--verbose)
-        CROSS_COMPILE_BUILD_VERBOSE=true
-        shift
-        ;;
-      -*)
-        dump_error "Unknown option $1"
-        ;;
-      *)
-        CROSS_COMPILE_BUILD_POSITIONAL_ARGS+=("$1")
-        shift
-        ;;
-    esac
-  done
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+            -t|--tool_dir)
+                CROSS_COMPILE_BUILD_TOOL_DIR="$2"
+                shift
+                shift
+                ;;
+            -b|--build_dir)
+                CROSS_COMPILE_BUILD_BUILD_DIR="$2"
+                shift
+                shift
+                ;;
+            -h|--help)
+                help
+                exit 0
+                ;;
+            -i|--install)
+                CROSS_COMPILE_BUILD_INSTALL_FOUND=true
+                shift
+                ;;
+            -v|--verbose)
+                CROSS_COMPILE_BUILD_VERBOSE=true
+                shift
+                ;;
+            -*)
+                dump_error "Unknown option $1"
+                ;;
+            *)
+                CROSS_COMPILE_BUILD_POSITIONAL_ARGS+=("$1")
+                shift
+                ;;
+        esac
+    done
 
-  set -- "${CROSS_COMPILE_BUILD_POSITIONAL_ARGS[@]}"
+    set -- "${CROSS_COMPILE_BUILD_POSITIONAL_ARGS[@]}"
 }
 
 process_args() {
-  if [ $CROSS_COMPILE_BUILD_INSTALL_FOUND = false ] ; then
-    dump_error "--install flag was not provided!"
-  fi
+    if [ $CROSS_COMPILE_BUILD_INSTALL_FOUND = false ] ; then
+        dump_error "--install flag was not provided!"
+    fi
 
-  if [ -z "${CROSS_COMPILE_BUILD_TOOL_DIR}" ] ; then
-    dump_error "--tool_dir | -t flag with directory where save tooling was not provided!"
-  fi
+    if [ -z "${CROSS_COMPILE_BUILD_TOOL_DIR}" ] ; then
+        dump_error "--tool_dir | -t flag with directory where save tooling was not provided!"
+    fi
 
-  if [ -z "${CROSS_COMPILE_BUILD_BUILD_DIR}" ] ; then
-    dump_error "--build_dir | -t flag with directory where save build files was not provided!"
-  fi
+    if [ -z "${CROSS_COMPILE_BUILD_BUILD_DIR}" ] ; then
+        dump_error "--build_dir | -t flag with directory where save build files was not provided!"
+    fi
 }
 
 main() {
-  parse_args "$@"
-  process_args
-  run_build
+    parse_args "$@"
+    process_args
+    run_build
 }
 
 main "$@"
