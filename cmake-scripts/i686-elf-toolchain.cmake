ca9c5fa1118ca641abb412403746cde8299ec630
Łukasz Kryczka <elek123456789101112@gmail.com>
2024-11-28 23:13:43 +0100

    CMakeLists.txt refactor

--- cmake-scripts/i686-elf-toolchain.cmake
+++ cmake-scripts/i686-elf-toolchain.cmake
@@ -2,18 +2,27 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR i686)
 
+# This makes CMake test the compiler by building a static library
+# (So it doesn't need to link an executable against C standard library)
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
 # Define the path to your cross-compiler tools
-set(CROSS_COMPILER_PATH "${CMAKE_SOURCE_DIR}/tools/bin")
+set(CROSS_COMPILER_PATH "${CMAKE_CURRENT_LIST_DIR}/../tools/bin")
 
 # Specify the cross-compilers
 set(CMAKE_C_COMPILER "${CROSS_COMPILER_PATH}/i686-elf-gcc")
 set(CMAKE_CXX_COMPILER "${CROSS_COMPILER_PATH}/i686-elf-g++")
 set(CMAKE_AR "${CROSS_COMPILER_PATH}/i686-elf-ar")
 set(CMAKE_RANLIB "${CROSS_COMPILER_PATH}/i686-elf-ranlib")
+set(CMAKE_LINKER "${CROSS_COMPILER_PATH}/i686-elf-ld")
+set(CMAKE_ASM_NASM_COMPILER "nasm")
+
+# Setup the cmake environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER) # Don't search for programs on the host system
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)  # Only search for libraries in the target directories
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)  # Only search for headers in the target directories
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)  # Only search for packages in the target directories
 
 # Optional: Specify NASM if it's not in your PATH
 # set(CMAKE_ASM_NASM_COMPILER "/path/to/your/nasm")
 
-# This makes CMake test the compiler by building a static library
-# (So it doesn't need to link an executable against C standard library)
-set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
--- scripts/build_alkos.bash
+++ scripts/build_alkos.bash
@@ -5,6 +5,8 @@ BUILD_SCRIPT_PATH="${BUILD_SCRIPT_DIR}/$(basename "$0")"
 
 BUILD_DIR="${BUILD_SCRIPT_DIR}/../build"
 SOURCE_DIR="${BUILD_SCRIPT_DIR}/.."
+CMAKE_TOOLCHAIN_FILE_NAME="i686-elf-toolchain.cmake"
+CMAKE_TOOLCHAIN_FILE_PATH="${SOURCE_DIR}/../cmake-scripts/${CMAKE_TOOLCHAIN_FILE_NAME}"
 
 source "${BUILD_SCRIPT_DIR}/../env/pretty_print.bash"
 source "${BUILD_SCRIPT_DIR}/../env/helpers.bash"
@@ -56,7 +58,7 @@ main() {
 
   mkdir -p "${BUILD_DIR}"
 
-  base_runner "Failed to configure AlkOS" "${VERBOSE}" cmake -B "${BUILD_DIR}" -S "${SOURCE_DIR}"
+  base_runner "Failed to configure AlkOS" "${VERBOSE}" cmake -B "${BUILD_DIR}" -S "${SOURCE_DIR}" -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TOOLCHAIN_FILE_PATH}"
   base_runner "Failed to build AlkOS" "${VERBOSE}" cmake --build "${BUILD_DIR}"
 
   pretty_success "AlkOS built successfully - Image is located in ${BUILD_DIR}/bin"
