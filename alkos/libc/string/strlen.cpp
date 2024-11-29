b05a35bed77451d5e16b531323045ea821d40508
Łukasz Kryczka <elek123456789101112@gmail.com>
2024-11-29 18:53:01 +0100

    Added clang-format

--- alkos/libc/string/strlen.cpp
+++ alkos/libc/string/strlen.cpp
@@ -1,9 +1,8 @@
 #include <stddef.h>
 
-size_t strlen(const char* str)
+size_t strlen(const char *str)
 {
     size_t len = 0;
-    while (str[len])
-        len++;
+    while (str[len]) len++;
     return len;
 }
