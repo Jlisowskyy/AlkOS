b46aa434f411badedbefcbc653a84c77ee508e4e
Jakub Lisowski <115403674+Jlisowskyy@users.noreply.github.com>
2024-12-16 21:11:16 +0100

    Cpp Support (#42)
    Things done in this commit:
    - added C++/C Global constructors initialisation
    - added static function's local variable initialisation stubs
    - added placement new operator
    - added new operators stubs
    - test verifying each of features mentioned
    
    Things that we consciously omit, but we understand will not work:
    - exceptions
    - RTTI
    - pure abstract methods

--- alkos/kernel/src/init.cpp
+++ alkos/kernel/src/init.cpp
@@ -3,6 +3,31 @@
 /* internal includes */
 #include <libssp.hpp>
 
-void KernelInit() {
+/* external init procedures */
+extern "C" void enable_osxsave();
+
+extern "C" void enable_sse();
+
+extern "C" void enable_avx();
+
+extern "C" void terminal_initialize();
+
+extern "C" void PreKernelInit() {
+    /**
+     * TODO:
+     * 1. Enable interrupts
+     * 2. GDT, IDT, TSS, etc.
+     */
+
+    /* NOTE: sequence is important */
+    enable_osxsave();
+    enable_sse();
+    enable_avx();
+
     __stack_chk_init();
+
+    terminal_initialize();
+}
+
+void KernelInit() {
 }
