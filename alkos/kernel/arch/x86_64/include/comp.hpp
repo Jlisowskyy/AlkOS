7739adba983b247352511746350fa9a5e59e529c
Jakub Lisowski <Jlisowskyy@gmail.com>
2024-12-16 22:41:14 +0100

    Initial changes

---
+++ alkos/kernel/arch/x86_64/include/comp.hpp
@@ -0,0 +1,12 @@
+#ifndef ARCH_X86_64_COMP_HPP_
+#define ARCH_X86_64_COMP_HPP_
+
+#ifdef __x86_64__
+
+#define DEF_MULTITARGET_FUNC(name,
+
+#else
+#error "not x86_64"
+#endif
+
+#endif // ARCH_X86_64_COMP_HPP_
