60a780e2805f9024f8dd4ad30b7ec5165af246e6
Jakub Lisowski <115403674+Jlisowskyy@users.noreply.github.com>
2025-01-02 16:24:12 +0100

    Jlisowskyy/traces (#69)
    Things done in this commit:
    - fixed small issues from previous commit
    - Added traces with snprintf functionality

--- alkos/kernel/include/debug.hpp
+++ alkos/kernel/include/debug.hpp
@@ -1,34 +1,40 @@
 #ifndef KERNEL_INCLUDE_DEBUG_HPP_
 #define KERNEL_INCLUDE_DEBUG_HPP_
 
-#include <defines.hpp>
-#include <todo.hpp>
+#include <stdio.h>
 #include <debug_terminal.hpp>
+#include <defines.hpp>
+#include <kernel_assert.hpp>
 
 // ------------------------------
 // Traces
 // ------------------------------
 
 /**
- * @brief TRACE - simple macro for debugging purposes, works only when __USE_DEBUG_OUTPUT__ and
- * __USE_DEBUG_TRACES__ are defined
- *
- * @note Currently supports using only compiled time strings
- *
- * @todo Add support for dynamic strings
+ * @brief TRACE - Macro to print some debug information only when build is instructed to.
+ * Works only when __USE_DEBUG_OUTPUT__ and __USE_DEBUG_TRACES__ are defined.
  */
 
-TODO_WHEN_SNPRINTF_EXISTS
+static constexpr size_t kTraceBufferSize = 4096;
+template <typename... Args>
+FAST_CALL constexpr void FormatTrace(const char* format, Args... args)
+{
+    char buffer[kTraceBufferSize];
+
+    const u64 bytesWritten = snprintf(buffer, kTraceBufferSize, format, args...);
+    ASSERT(bytesWritten < kTraceBufferSize);
+    DebugTerminalWrite(buffer);
+}
 
 #ifdef __USE_DEBUG_TRACES__
 
-#define TRACE(message) DebugTerminalWrite(message)
+#define TRACE(message, ...) FormatTrace(message __VA_OPT__(, ) __VA_ARGS__)
 
 #else
 
-#define TRACE(message)
+#define TRACE(message, ...)
 
-#endif // __USE_DEBUG_TRACES__
+#endif  // __USE_DEBUG_TRACES__
 
 #define TRACE_FORMAT_LOCATION(message) __FILE__ " " TOSTRING(__LINE__) " " message "\n"
 #define TRACE_FORMAT_ERROR(message)    "[ERROR] " TRACE_FORMAT_LOCATION(message)
@@ -36,9 +42,9 @@ TODO_WHEN_SNPRINTF_EXISTS
 #define TRACE_FORMAT_INFO(message)     "[INFO] " TRACE_FORMAT_LOCATION(message)
 #define TRACE_FORMAT_SUCCESS(message)  "[SUCCESS] " TRACE_FORMAT_LOCATION(message)
 
-#define TRACE_ERROR(message)   TRACE(TRACE_FORMAT_ERROR(message))
-#define TRACE_WARNING(message) TRACE(TRACE_FORMAT_WARNING(message))
-#define TRACE_INFO(message)    TRACE(TRACE_FORMAT_INFO(message))
-#define TRACE_SUCCESS(message) TRACE(TRACE_FORMAT_SUCCESS(message))
+#define TRACE_ERROR(message, ...)   TRACE(TRACE_FORMAT_ERROR(message) __VA_OPT__(, ) __VA_ARGS__)
+#define TRACE_WARNING(message, ...) TRACE(TRACE_FORMAT_WARNING(message) __VA_OPT__(, ) __VA_ARGS__)
+#define TRACE_INFO(message, ...)    TRACE(TRACE_FORMAT_INFO(message) __VA_OPT__(, ) __VA_ARGS__)
+#define TRACE_SUCCESS(message, ...) TRACE(TRACE_FORMAT_SUCCESS(message) __VA_OPT__(, ) __VA_ARGS__)
 
-#endif // KERNEL_INCLUDE_DEBUG_HPP_
+#endif  // KERNEL_INCLUDE_DEBUG_HPP_
