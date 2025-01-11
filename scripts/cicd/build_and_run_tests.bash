46a76746f4741f1c4a0d6aba5cae761ff2a5e661
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-01-11 19:52:36 +0100

    Added simple workflow

--- scripts/cicd/build_and_run_tests.bash
+++ scripts/cicd/build_and_run_tests.bash
@@ -3,11 +3,10 @@
 BUILD_AND_RUN_TESTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Step 1: Get requirements
-"${BUILD_AND_RUN_TESTS_DIR}/../env/install_deps_arch.bash" --install -v
+"${BUILD_AND_RUN_TESTS_DIR}/../env/install_deps_ubuntu.bash" --install -v
 
 # Step 2: Configure the environment
-# TODO: temporarly x86_64 is hardcoded
-"${BUILD_AND_RUN_TESTS_DIR}/../configure.bash" x86_64 debug_qemu_tests -v
+"${BUILD_AND_RUN_TESTS_DIR}/../configure.bash" "${1}" debug_qemu_tests -v
 
 # Step 3: Build toolchain
 "${BUILD_AND_RUN_TESTS_DIR}/../actions/install_toolchain.bash" -v
