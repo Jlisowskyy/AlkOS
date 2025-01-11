fcf5ab8bcd2c2d069423d5e2b1cbe9ddf514daff
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-01-11 13:40:31 +0100

    next

--- scripts/install/make_iso.bash
+++ scripts/install/make_iso.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Script metadata
 MAKE_ISO_SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 MAKE_ISO_SCRIPT_PATH="${MAKE_ISO_SCRIPT_DIR}/$(basename "$0")"
+
+# GRUB config placeholders and default values
 MAKE_ISO_SCRIPT_BOOTABLE_TOKEN="BOOTABLE_KERNEL_PLACEHOLDER"
 MAKE_ISO_SCRIPT_MODULES_TOKEN="MODULES_PLACEHOLDER"
-
 MAKE_ISO_SCRIPT_GRUB_CONTENTS="
 set timeout=0
 set default=0
@@ -17,37 +19,31 @@ menuentry \"AlkOS\" {
 MAKE_ISO_SCRIPT_GRUB_PATH_IN_ISO="boot/grub/grub.cfg"
 MAKE_ISO_SCRIPT_EXECUTABLE_NAME="alkos.kernel"
 MAKE_ISO_SCRIPT_MODULES_LIST=""
+MAKE_ISO_SCRIPT_TARGET=""
+MAKE_ISO_SCRIPT_SOURCE=""
+MAKE_ISO_SCRIPT_VERBOSE=false
 
-
+# Helper functions
 source "${MAKE_ISO_SCRIPT_DIR}/../utils/helpers.bash"
 source "${MAKE_ISO_SCRIPT_DIR}/../utils/pretty_print.bash"
 
 help() {
-  echo "${MAKE_ISO_SCRIPT_PATH} [target] [source] [--exec_name | -e executable_name] [--modules | -m modules]
-   [--run | -r] [--verbose | -v]"
+  echo "${MAKE_ISO_SCRIPT_PATH} <target> <source> [--exec_name | -e executable_name] [--modules | -m modules] [--run | -r] [--verbose | -v]"
   echo "Creates a .iso for alkOS from the sysroot directory"
   echo "Where:"
-  echo "target - path to the .iso file to create"
-  echo "source - path to the sysroot directory of alkOS"
-  echo "--exec_name | -e - name of the executable in sysroot/boot to boot (default: alkos.kernel)"
-  echo "--modules  | -m - list of modules in sysroot/boot to load along with the kernel"
-  echo "The list should be a space separated list of module names in quotes eg. \"module1 module2\""
-  echo "--run     | -r - flag to run make_iso.sh"
-  echo "--verbose | -v - flag to enable verbose output"
+  echo "  target  - Path to the .iso file to create"
+  echo "  source  - Path to the sysroot directory of alkOS"
+  echo "  --exec_name | -e - Name of the executable in sysroot/boot to boot (default: alkos.kernel)"
+  echo "  --modules  | -m - Space-separated list of modules in sysroot/boot to load with the kernel"
+  echo "  --verbose  | -v - Enable verbose output"
 }
 
 parse_args() {
-  MAKE_ISO_SCRIPT_RUN=false
-  MAKE_ISO_SCRIPT_VERBOSE=false
   while [[ $# -gt 0 ]]; do
     case $1 in
       -h|--help)
         help
         exit 0
-        ;;
-      -r|--run)
-        MAKE_ISO_SCRIPT_RUN=true
-        shift
         ;;
       -v|--verbose)
         MAKE_ISO_SCRIPT_VERBOSE=true
@@ -55,21 +51,10 @@ parse_args() {
         ;;
       -e|--exec_name)
         MAKE_ISO_SCRIPT_EXECUTABLE_NAME="$2"
-        MAKE_ISO_SCRIPT_GRUB_CONTENTS="${MAKE_ISO_SCRIPT_GRUB_CONTENTS//${MAKE_ISO_SCRIPT_BOOTABLE_TOKEN}/${MAKE_ISO_SCRIPT_EXECUTABLE_NAME}}"
         shift 2
         ;;
       -m|--modules)
         MAKE_ISO_SCRIPT_MODULES_LIST="$2"
-
-        local MODULES_LINES=""
-        for module in ${MAKE_ISO_SCRIPT_MODULES_LIST}; do
-          MODULES_LINES+="  module2 /boot/${module}\n"
-        done
-
-        # Convert the \n to actual newlines and strip spaces at the start of the first line and only it
-        local FORMATTED_MODULES
-        FORMATTED_MODULES=$(echo -e "$MODULES_LINES" | sed '1s/^[[:space:]]*//')
-        MAKE_ISO_SCRIPT_GRUB_CONTENTS="${MAKE_ISO_SCRIPT_GRUB_CONTENTS//${MAKE_ISO_SCRIPT_MODULES_TOKEN}/${FORMATTED_MODULES}}"
         shift 2
         ;;
       *)
@@ -89,31 +74,36 @@ parse_args() {
 
 process_args() {
   if [ -z "$MAKE_ISO_SCRIPT_TARGET" ] || [ -z "$MAKE_ISO_SCRIPT_SOURCE" ]; then
-    dump_error "Both target and source must be provided!"
+    dump_error "Both target and source paths must be provided!"
+    help
     exit 1
   fi
 
-  if [ "$MAKE_ISO_SCRIPT_RUN" = false ] ; then
-    dump_error "--run flag was not provided!"
-    exit 1
+  # Replace the kernel executable placeholder in the GRUB contents
+  MAKE_ISO_SCRIPT_GRUB_CONTENTS="${MAKE_ISO_SCRIPT_GRUB_CONTENTS//${MAKE_ISO_SCRIPT_BOOTABLE_TOKEN}/${MAKE_ISO_SCRIPT_EXECUTABLE_NAME}}"
+
+  # Replace the modules placeholder in the GRUB contents
+  if [ -n "$MAKE_ISO_SCRIPT_MODULES_LIST" ]; then
+    local MODULES_LINES=""
+    for module in $MAKE_ISO_SCRIPT_MODULES_LIST; do
+      MODULES_LINES+="  module2 /boot/${module}\n"
+    done
+    MAKE_ISO_SCRIPT_GRUB_CONTENTS="${MAKE_ISO_SCRIPT_GRUB_CONTENTS//${MAKE_ISO_SCRIPT_MODULES_TOKEN}/$(echo -e "${MODULES_LINES}")}"
+  else
+    MAKE_ISO_SCRIPT_GRUB_CONTENTS="${MAKE_ISO_SCRIPT_GRUB_CONTENTS//${MAKE_ISO_SCRIPT_MODULES_TOKEN}/}"
   fi
 
-  if [ -z "$MAKE_ISO_SCRIPT_EXECUTABLE_NAME" ]; then
-    dump_error "Executable name must be provided!"
-    exit 1
-  fi
-
-  if [[ $MAKE_ISO_SCRIPT_GRUB_CONTENTS == *"${MAKE_ISO_SCRIPT_BOOTABLE_TOKEN}"* ]] ||
-     [[ $MAKE_ISO_SCRIPT_GRUB_CONTENTS == *"${MAKE_ISO_SCRIPT_MODULES_TOKEN}"* ]]; then
-    dump_error "BOOTABLE_TOKEN or MODULES_TOKEN are still present in the grub contents!"
+  # Check if the GRUB contents are valid
+  if [[ "$MAKE_ISO_SCRIPT_GRUB_CONTENTS" == *"${MAKE_ISO_SCRIPT_BOOTABLE_TOKEN}"* ]] ||
+     [[ "$MAKE_ISO_SCRIPT_GRUB_CONTENTS" == *"${MAKE_ISO_SCRIPT_MODULES_TOKEN}"* ]]; then
+    dump_error "Failed to replace tokens in the GRUB configuration!"
     exit 1
   fi
 
   if [ "$MAKE_ISO_SCRIPT_VERBOSE" = true ]; then
-    pretty_info "Grub config contents: $MAKE_ISO_SCRIPT_GRUB_CONTENTS"
+    pretty_info "Grub configuration contents:\n$MAKE_ISO_SCRIPT_GRUB_CONTENTS"
   fi
 }
-
 
 main() {
   parse_args "$@"
@@ -136,12 +126,12 @@ main() {
   base_runner "Failed to create grub.cfg" "${MAKE_ISO_SCRIPT_VERBOSE}" \
     echo "${MAKE_ISO_SCRIPT_GRUB_CONTENTS}" > "${MAKE_ISO_SCRIPT_SOURCE}/${MAKE_ISO_SCRIPT_GRUB_PATH_IN_ISO}"
 
-  pretty_info "Creating .iso file $MAKE_ISO_SCRIPT_TARGET from $MAKE_ISO_SCRIPT_SOURCE"
-  pretty_info "Ensuring that the path to the .iso file exists"
+  pretty_info "Creating .iso file: $MAKE_ISO_SCRIPT_TARGET from source: $MAKE_ISO_SCRIPT_SOURCE"
   base_runner "Failed to create path to .iso file" "${MAKE_ISO_SCRIPT_VERBOSE}" mkdir -p "$(dirname "${MAKE_ISO_SCRIPT_TARGET}")"
   base_runner "Failed to create .iso file" "${MAKE_ISO_SCRIPT_VERBOSE}" \
     grub-mkrescue -o "${MAKE_ISO_SCRIPT_TARGET}" "${MAKE_ISO_SCRIPT_SOURCE}"
-  pretty_info "Created .iso file $(basename "${MAKE_ISO_SCRIPT_TARGET}")"
+
+  pretty_success "Created .iso file: $(basename "${MAKE_ISO_SCRIPT_TARGET}")"
 }
 
 main "$@"
